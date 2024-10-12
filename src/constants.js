b0467ebf7e4d505579c3a1fa1b94153b90b6a9d0
mathewfonteyne <111210884+mathewfonteyne@users.noreply.github.com>
2024-10-12 18:01:53 -0400

    pushing my project to this repo

---
+++ src/constants.js
@@ -0,0 +1,17 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. When I'm not using it for work or coding, I use it for Video Games.
+      Some of my coding projects can be found on my <a href="https://github.com/mathewfonteyne" target="_blank">Github</a>!`,
+  "cs-degree": `This is my Software Development certificate. While it may seem insignificant, the course gave me a solid foundation on which I can build and enhance my coding skills.`,
+  "sofa-table": `That's my sofa. I like to relax here and watch YouTube.`,
+  tv: `That's my TV. I've been watching tech YouTubers a lot recently like :
+    <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> and <a href="https://www.youtube.com/@professormesser" target="_blank">Professor Messor</a>!`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `This is my desk and on it is my resume. <a href="https://github.com/mathewfonteyne/My-Resume/blob/main/My%20Resume.pdf" target="_blank">Check it out?</a>
+    Contact me by email at <a href="mailto: matfontech@gmail.com">matfontech@gmail.com</a>, or connect with me on <a href= "https://www.linkedin.com/in/mathewfonteyne/" target="_blank">LinkedIn</a>.`,
+  projects: `Info about this portfolio: It's made with the Kaboom.js library which is a library for making games in JavaScript.
+    Text is rendered with HTML/CSS.`,
+  library: `There are way too many books on these shelves. I'll probably never read them all.`,
+  exit: `If you want to exit Mathew Fonteyne's portfolio, just close the tab.`,
+};
